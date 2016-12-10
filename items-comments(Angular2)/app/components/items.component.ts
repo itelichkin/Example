d292769e872d3d9ad8f01453179c5cf1545e18e8
itelichkin <itelichkin@gmail.com>
2016-12-10 19:16:47 +0200

    add items-comments(Angular2)

---
+++ items-comments(Angular2)/app/components/items.component.ts
@@ -0,0 +1,8 @@
+import {Component} from "@angular/core";
+@Component({
+    moduleId: module.id,
+    selector: 'items-app',
+    templateUrl:'items.component.html',
+    styleUrls:['items.component.css']
+})
+export class ItemsComponent{}
